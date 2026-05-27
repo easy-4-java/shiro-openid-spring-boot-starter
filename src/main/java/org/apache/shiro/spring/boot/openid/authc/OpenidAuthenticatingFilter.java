e1e9eb569dbca763097d435cb20983025d33f5a5
wandl <hiwepy@163.com>
2026-05-27 11:35:45 +0800

    chore(pom): align to Spring Boot 3.5.x (JDK 17, Boot 3.5.9)

--- src/main/java/org/apache/shiro/spring/boot/openid/authc/OpenidAuthenticatingFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/openid/authc/OpenidAuthenticatingFilter.java
@@ -17,10 +17,10 @@ package org.apache.shiro.spring.boot.openid.authc;
 
 import java.util.List;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.biz.authc.AuthcResponse;
