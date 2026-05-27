29998009381ad4ca11df71286fcacf203d8cadf2
wandl <hiwepy@163.com>
2026-05-27 11:35:25 +0800

    chore(pom): align to Spring Boot 3.2.x (JDK 17, Boot 3.2.12)

--- src/main/java/org/apache/shiro/spring/boot/openid/OpenidDiscoveryInformationSessionProvider.java
+++ src/main/java/org/apache/shiro/spring/boot/openid/OpenidDiscoveryInformationSessionProvider.java
@@ -15,8 +15,8 @@
  */
 package org.apache.shiro.spring.boot.openid;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 
 import org.apache.shiro.biz.utils.SubjectUtils;
 import org.apache.shiro.session.Session;
